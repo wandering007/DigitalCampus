3518d1e71843e12a96abb74a261c69b8d3b7222a
wandering007 <uestcdaniel@gmail.com>
2015-05-07 16:19:49 +0800

    implementation
    based on route-track Dijkstra algorithm

--- digital_campus.cpp
+++ digital_campus.cpp
@@ -8,8 +8,6 @@ int main()
 
 /*************************************
  *Name:Changmao Cheng                *
- *Student ID: 2012030040006          *
- *Class ID: 2012001020               *
  *School: Yingcai Experimental School*
  *Date: 2014/6/21                    *
  *************************************/
--- path.cpp
+++ path.cpp
@@ -306,8 +306,6 @@ start_5:
 
 /*************************************
  *Name:Changmao Cheng                *
- *Student ID: 2012030040006          *
- *Class ID: 2012001020               *
  *School: Yingcai Experimental School*
  *Date: 2014/6/21                    *
  *************************************/
