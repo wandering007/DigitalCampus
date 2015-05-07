ee4cfc29757349981aebe448697b9099d05ae663
wandering007 <uestcdaniel@gmail.com>
2015-05-07 16:19:56 +0800

    Revert "implementation"
    This reverts commit 3518d1e71843e12a96abb74a261c69b8d3b7222a.

--- digital_campus.cpp
+++ digital_campus.cpp
@@ -8,6 +8,8 @@ int main()
 
 /*************************************
  *Name:Changmao Cheng                *
+ *Student ID: 2012030040006          *
+ *Class ID: 2012001020               *
  *School: Yingcai Experimental School*
  *Date: 2014/6/21                    *
  *************************************/
--- path.cpp
+++ path.cpp
@@ -306,6 +306,8 @@ start_5:
 
 /*************************************
  *Name:Changmao Cheng                *
+ *Student ID: 2012030040006          *
+ *Class ID: 2012001020               *
  *School: Yingcai Experimental School*
  *Date: 2014/6/21                    *
  *************************************/
